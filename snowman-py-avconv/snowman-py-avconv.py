899661b4680b6ce4dddda50da66386f9f8948993
Bernard Ladenthin <bernard@ladenthin.net>
2014-06-29 23:00:22 +0200

    improve avconv script, use two videos for an archive, improve settings

--- snowman-py-avconv/snowman-py-avconv.py
+++ snowman-py-avconv/snowman-py-avconv.py
@@ -65,10 +65,14 @@ if not options.targetPath:
     parser.error("targetPath required.")
 
 srcFile=options.srcPath+"/"+"%d.jpeg"
-targetFile=options.targetPath+"/"+options.date+".mp4"
+targetFileHighRes=options.targetPath+"/"+options.date+"_HD"+".mp4"
+targetFileLowRes=options.targetPath+"/"+options.date+".mp4"
+
+# http://dev.beandog.org/x264_preset_reference.html
+# http://mewiki.project357.com/wiki/X264_Settings
 
 #avconv [input options] -i [input filename] -codec:v [video options] -codec:a [audio options] [output file options] [output filename]
-avconvexec=[
+avconvexecHighRes=[
     "avconv",
 #overwrite if file exist
     "-y",
@@ -78,30 +82,55 @@ avconvexec=[
 #encode video to H.264 using libx264 library
     "-codec:v",
     "libx264",
+#crf Constant Rate Factor http://slhck.info/articles/crf
+    "-crf",
+    "26",
 #sets encoding preset for x264
 #ultrafast, superfast, veryfast, faster, fast, medium, slow, slower, veryslow
     "-preset",
-    "medium",
-#sets video bitrate in bits/s
-    "-b:v",
-    "500k",
+    "slow",
+#sets video bitrate in bits/s (not used, we use crf)
+#    "-b:v",
+#    "500k",
 #maxrate and -bufsize forces libx264 to build video in a way,
 #that it could be streamed over 500kbit/s line considering device buffer of 1000kbits.
-    "-maxrate",
-    "500k",
-    "-bufsize",
-    "1000k",
+#educes quality, only use this if you're encoding for a playback scenario that requires it.
+#    "-maxrate",
+#    "500k",
+#    "-bufsize",
+#    "1000k",
 #scale filter
 #-1 means: resize so the aspect ratio is same.
 #    "-vf",
 #    "scale=-1:360",
+#use all threads
     "-threads",
     "0",
 #disable audio
     "-an",
-    targetFile
+    targetFileHighRes
 ]
 
-call(avconvexec)
+avconvexecLowRes=[
+    "avconv",
+    "-y",
+    "-i",
+    srcFile,
+    "-codec:v",
+    "libx264",
+    "-crf",
+    "35",
+    "-preset",
+    "medium",
+    "-threads",
+    "0",
+    "-an",
+    targetFileLowRes
+]
+
+#first create the low resolution video
+call(avconvexecLowRes)
+call(avconvexecHighRes)
+
 removeDir(options.srcPath)
 
