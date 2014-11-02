556a942dfe133012fd9bbd35e43c846222dabe01
Bernard Ladenthin <bernard.ladenthin@gmail.com>
2014-11-02 16:39:14 +0100

    improve snowman-html5-canvasapi, release snowman-html5-canvasapi version 1.0.1

--- snowman-html5-canvasapi/snowman-html5-canvasapi.js
+++ snowman-html5-canvasapi/snowman-html5-canvasapi.js
@@ -1,3 +1,7 @@
+/*jslint devel: true, browser: true */
+/*global jQuery*/
+// test this file with http://jshint.com/ and http://jshint.com/
+// version 1.0.1
 /**
  * snowman-html5-canvasapi- HTML5 canvas api for snowman.
  * https://github.com/bernardladenthin/snowman
@@ -10,197 +14,193 @@
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  */
-
 function getSnowmanHtml5Canvasapi() {
+    "use strict";
 
-	snowmanHtml5Canvasapi = {
-		"bDebugLog" : false,
-		"data" : {
-			"canvas" : false,
-			"liveviewLastSeconds" : 0,
-			"liveviewLastMillis" : 0
-		}
-	};
+    var snowmanHtml5Canvasapi = {
+        "bDebugLog" : false,
+        "data" : {
+            "canvas" : false,
+            "liveviewLastSeconds" : 0,
+            "liveviewLastMillis" : 0
+        }
+    };
 
-	snowmanHtml5Canvasapi.debugLog = function(o) {
-		if(snowmanHtml5Canvasapi.bDebugLog) {
-			console.log(o);
-		}
-	};
+    snowmanHtml5Canvasapi.debugLog = function (o) {
+        if (snowmanHtml5Canvasapi.bDebugLog) {
+            console.log(o);
+        }
+    };
 
-	snowmanHtml5Canvasapi.enableDebugLog = function() {
-		snowmanHtml5Canvasapi.bDebugLog = true;
-	};
+    snowmanHtml5Canvasapi.enableDebugLog = function () {
+        snowmanHtml5Canvasapi.bDebugLog = true;
+    };
 
-	snowmanHtml5Canvasapi.disableDebugLog = function() {
-		snowmanHtml5Canvasapi.bDebugLog = false;
-	};
+    snowmanHtml5Canvasapi.disableDebugLog = function () {
+        snowmanHtml5Canvasapi.bDebugLog = false;
+    };
 
-	snowmanHtml5Canvasapi.getCanvas = function() {
-		return snowmanHtml5Canvasapi.data.canvas;
-	};
+    snowmanHtml5Canvasapi.getCanvas = function () {
+        return snowmanHtml5Canvasapi.data.canvas;
+    };
 
-	snowmanHtml5Canvasapi.setCanvas = function(canvas) {
-		snowmanHtml5Canvasapi.data.canvas = canvas;
-	};
+    snowmanHtml5Canvasapi.setCanvas = function (canvas) {
+        snowmanHtml5Canvasapi.data.canvas = canvas;
+    };
 
-	/**
-	 * XmlHttpRequest's getAllResponseHeaders() method returns a
-	 * string of response headers according to the format described here:
-	 * http://www.w3.org/TR/XMLHttpRequest/#the-getallresponseheaders-method
-	 * This method parses that string into
-	 * a user-friendly key/value pair object.
-	 * from https://gist.github.com/monsur/706839
-	 */
-	snowmanHtml5Canvasapi.parseResponseHeaders = function(headerStr) {
-		var headers = {};
-			if (!headerStr) {
-			return headers;
-		}
+    /**
+     * XmlHttpRequest's getAllResponseHeaders() method returns a
+     * string of response headers according to the format described here:
+     * http://www.w3.org/TR/XMLHttpRequest/#the-getallresponseheaders-method
+     * This method parses that string into
+     * a user-friendly key/value pair object.
+     * from https://gist.github.com/monsur/706839
+     */
+    snowmanHtml5Canvasapi.parseResponseHeaders = function (headerStr) {
+        var headers, pairs, headerPair, index, k, v, i;
 
-		var pairs = headerStr.split('\u000d\u000a');
-		for (var i = 0; i < pairs.length; i++) {
-			var headerPair = pairs[i];
-			var index = headerPair.indexOf('\u003a\u0020');
-			if (index > 0) {
-				var k = headerPair.substring(0, index);
-				var v = headerPair.substring(index + 2);
-				headers[k] = v;
-			}
-		}
-		return headers;
-	};
+        headers = {};
+        pairs = headerStr.split("\r\n");
 
-	snowmanHtml5Canvasapi.initCanvas = function() {
-		canvas = snowmanHtml5Canvasapi.getCanvas();
-		if (canvas[0].getContext) {
-			width=250;
-			height=250;
+        if (!headerStr) {
+            return headers;
+        }
 
-			canvas[0].width=width;
-			canvas[0].height=height;
-			var ctx = canvas[0].getContext("2d");
+        for (i = 0; i < pairs.length; i = i + 1) {
+            headerPair = pairs[i];
+            index = headerPair.indexOf(": ");
+            if (index > 0) {
+                k = headerPair.substring(0, index);
+                v = headerPair.substring(index + 2);
+                headers[k] = v;
+            }
+        }
+        return headers;
+    };
 
-			ctx.moveTo(0,0);
-			ctx.lineTo(width,height);
-			ctx.stroke();
+    snowmanHtml5Canvasapi.initCanvas = function () {
+        var canvas = snowmanHtml5Canvasapi.getCanvas(), width, height, ctx;
+        if (canvas[0].getContext) {
+            width = 250;
+            height = 250;
 
-			ctx.fillStyle = "black";
-			ctx.strokeRect(0,0,width,height);
-		}
-	};
+            canvas[0].width = width;
+            canvas[0].height = height;
+            ctx = canvas[0].getContext("2d");
 
-	snowmanHtml5Canvasapi.fetchImage = function(name, url, additionalData) {
-		time = new Date();
-		timestamp = time.getTime();
-		data =
-			'name='+name
-		+	'&t='+timestamp
-		+	'&base64=true';
+            ctx.moveTo(0, 0);
+            ctx.lineTo(width, height);
+            ctx.stroke();
 
-		if(additionalData && additionalData.length > 0) {
-			data += additionalData;
-		}
+            ctx.fillStyle = "black";
+            ctx.strokeRect(0, 0, width, height);
+        }
+    };
 
-		jQuery.ajax({
-			type: "GET",
-			url: url,
-			data: data,
-			dataType: "text",
-			success: snowmanHtml5Canvasapi.successCb,
-			error: snowmanHtml5Canvasapi.errorCb,
-			complete: snowmanHtml5Canvasapi.completeCb
-		});
-	};
+    snowmanHtml5Canvasapi.fetchImage = function (name, url, additionalData) {
+        var time, timestamp, data;
 
-	//TODO: add here an information callback
-/*
-function connect() {
-	hostname = arguments[0] || "localhost";
-	port = port arguments[1] || 80;
-	method = arguments[2] || "GET";
-}
-*/
-	snowmanHtml5Canvasapi.successCb = function(data, textStatus, jqXHR) {
-		snowmanHtml5Canvasapi.debugLog("call snowmanHtml5Canvasapi.successCb");
-		snowmanHtml5Canvasapi.debugLog(textStatus);
-		snowmanHtml5Canvasapi.debugLog(jqXHR);
+        time = new Date();
+        timestamp = time.getTime();
+        data = 'name=' + name + '&t=' + timestamp + '&base64=true';
 
-		snowmanHtml5Canvasapi.debugLog(jqXHR.getAllResponseHeaders());
-		header = snowmanHtml5Canvasapi.parseResponseHeaders(
-			jqXHR.getAllResponseHeaders()
-		);
-		snowmanHtml5Canvasapi.debugLog(header);
+        if (additionalData && additionalData.length > 0) {
+            data += additionalData;
+        }
 
-		seconds = parseInt(header["snowman-timeseconds"]);
-		millis = parseInt(header["snowman-timemillis"]);
+        jQuery.ajax({
+            type: "GET",
+            url: url,
+            data: data,
+            dataType: "text",
+            success: snowmanHtml5Canvasapi.successCb,
+            error: snowmanHtml5Canvasapi.errorCb,
+            complete: snowmanHtml5Canvasapi.completeCb
+        });
+    };
 
-		snowmanHtml5Canvasapi.debugLog(seconds);
+    snowmanHtml5Canvasapi.successCb = function (data, textStatus, jqXHR) {
+        var header, seconds, millis, hit, image, ctx, canvas;
+        snowmanHtml5Canvasapi.debugLog("call snowmanHtml5Canvasapi.successCb");
+        snowmanHtml5Canvasapi.debugLog(textStatus);
+        snowmanHtml5Canvasapi.debugLog(jqXHR);
 
-		snowmanHtml5Canvasapi.debugLog(millis);
+        snowmanHtml5Canvasapi.debugLog(jqXHR.getAllResponseHeaders());
+        header = snowmanHtml5Canvasapi.parseResponseHeaders(
+            jqXHR.getAllResponseHeaders()
+        );
+        snowmanHtml5Canvasapi.debugLog(header);
 
-		snowmanHtml5Canvasapi.debugLog(
-			snowmanHtml5Canvasapi.data.liveviewLastSeconds
-		);
+        seconds = parseInt(header["snowman-timeseconds"], 10);
+        millis = parseInt(header["snowman-timemillis"], 10);
 
-		snowmanHtml5Canvasapi.debugLog(
-			snowmanHtml5Canvasapi.data.liveviewLastMillis
-		);
+        snowmanHtml5Canvasapi.debugLog(seconds);
 
-		hit = false;
+        snowmanHtml5Canvasapi.debugLog(millis);
 
-		if(snowmanHtml5Canvasapi.data.liveviewLastSeconds < seconds) {
-			hit = true;
-		} else if(
-				snowmanHtml5Canvasapi.data.liveviewLastSeconds == seconds
-			&&	snowmanHtml5Canvasapi.data.liveviewLastMillis < millis
-		) {
-			hit = true;
-		} else if(isNaN(seconds) || isNaN(millis)) {
-			seconds = millis = 0;
-			//no snowman-* information could be found
-			hit = true;
-		}
+        snowmanHtml5Canvasapi.debugLog(
+            snowmanHtml5Canvasapi.data.liveviewLastSeconds
+        );
 
-		if(hit) {
-			snowmanHtml5Canvasapi.data.liveviewLastSeconds = seconds;
-			snowmanHtml5Canvasapi.data.liveviewLastMillis = millis;
+        snowmanHtml5Canvasapi.debugLog(
+            snowmanHtml5Canvasapi.data.liveviewLastMillis
+        );
 
-			snowmanHtml5Canvasapi.debugLog("hit");
+        hit = false;
 
-			var canvas = snowmanHtml5Canvasapi.getCanvas();
-			if (canvas[0].getContext) {
+        if (snowmanHtml5Canvasapi.data.liveviewLastSeconds < seconds) {
+            hit = true;
+        } else if (
+            // use === instead == because seconds are already an int
+            snowmanHtml5Canvasapi.data.liveviewLastSeconds === seconds &&
+                snowmanHtml5Canvasapi.data.liveviewLastMillis < millis
+        ) {
+            hit = true;
+        } else if (isNaN(seconds) || isNaN(millis)) {
+            seconds = millis = 0;
+            //no snowman-* information could be found
+            hit = true;
+        }
 
-				var image = new Image();
-				image.src = "data:image/jpeg;base64,"+data;
-				image.onload = function() {
+        if (hit) {
+            snowmanHtml5Canvasapi.data.liveviewLastSeconds = seconds;
+            snowmanHtml5Canvasapi.data.liveviewLastMillis = millis;
 
-					canvas[0].width=image.naturalWidth;
-					canvas[0].height=image.naturalHeight;
+            snowmanHtml5Canvasapi.debugLog("hit");
 
-					var ctx = canvas[0].getContext("2d");
-					ctx.drawImage(image,0,0);
+            canvas = snowmanHtml5Canvasapi.getCanvas();
+            if (canvas[0].getContext) {
 
-				};
-			}
-		} else {
-			snowmanHtml5Canvasapi.debugLog("miss");
-		}
-	};
+                image = new Image();
+                image.src = "data:image/jpeg;base64," + data;
+                image.onload = function () {
 
-	snowmanHtml5Canvasapi.errorCb = function(jqXHR, textStatus, errorThrown) {
-		snowmanHtml5Canvasapi.debugLog("call liveviewErrorCb");
-		snowmanHtml5Canvasapi.debugLog(jqXHR);
-		snowmanHtml5Canvasapi.debugLog(textStatus);
-		snowmanHtml5Canvasapi.debugLog(errorThrown);
-	};
+                    canvas[0].width = image.naturalWidth;
+                    canvas[0].height = image.naturalHeight;
 
-	snowmanHtml5Canvasapi.completeCb = function(jqXHR, textStatus) {
-		snowmanHtml5Canvasapi.debugLog("call liveviewCompleteCb");
-		snowmanHtml5Canvasapi.debugLog(jqXHR);
-		snowmanHtml5Canvasapi.debugLog(textStatus);
-	};
+                    ctx = canvas[0].getContext("2d");
+                    ctx.drawImage(image, 0, 0);
 
-	return snowmanHtml5Canvasapi;
+                };
+            }
+        } else {
+            snowmanHtml5Canvasapi.debugLog("miss");
+        }
+    };
+
+    snowmanHtml5Canvasapi.errorCb = function (jqXHR, textStatus, errorThrown) {
+        snowmanHtml5Canvasapi.debugLog("call liveviewErrorCb");
+        snowmanHtml5Canvasapi.debugLog(jqXHR);
+        snowmanHtml5Canvasapi.debugLog(textStatus);
+        snowmanHtml5Canvasapi.debugLog(errorThrown);
+    };
+
+    snowmanHtml5Canvasapi.completeCb = function (jqXHR, textStatus) {
+        snowmanHtml5Canvasapi.debugLog("call liveviewCompleteCb");
+        snowmanHtml5Canvasapi.debugLog(jqXHR);
+        snowmanHtml5Canvasapi.debugLog(textStatus);
+    };
+
+    return snowmanHtml5Canvasapi;
 }
 
